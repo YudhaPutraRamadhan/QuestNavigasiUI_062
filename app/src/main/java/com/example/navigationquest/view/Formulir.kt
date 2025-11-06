1716c3368ee0ca7358ba9d1317f8a4b05fd2db00
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-06 15:44:18 +0900

    Menambahkan button Submit pada halaman formulir

--- app/src/main/java/com/example/navigationquest/view/Formulir.kt
+++ app/src/main/java/com/example/navigationquest/view/Formulir.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -87,6 +89,12 @@ fun FormIsian(
                     onValueChange = {},
                 )
                 Spacer(modifier = Modifier.height(30.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(1f),
+                    onClick = OnSubmitBtnClick
+                ) {
+                    
+                }
             }
     }
 }
