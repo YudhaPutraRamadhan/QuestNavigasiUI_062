7220b8dabb88f325c8498138009c141d176bb1ad
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-06 15:11:00 +0900

    Menambahkan dependensi Material3 dan mengimplementasikan Scaffold dengan TopAppBar

--- app/src/main/java/com/example/navigationquest/view/Formulir.kt
+++ app/src/main/java/com/example/navigationquest/view/Formulir.kt
@@ -1,11 +1,22 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.navigationquest.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
-    jenisK:List<String> = listOf("Laki-Laki","Peremupuan"),
+    jenisK:List<String> = listOf("Laki-Laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
-) {
+){
+    Scaffold (modifier= Modifier,
+        {
+            TopAppBar(
 
+            )
+        })
 }
