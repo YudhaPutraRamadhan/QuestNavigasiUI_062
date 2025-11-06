f6bade788b280217e082274097f8709fd0e89fbe
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-06 15:27:33 +0900

    Menambahkan Column pada composable FormIsian

--- app/src/main/java/com/example/navigationquest/view/Formulir.kt
+++ app/src/main/java/com/example/navigationquest/view/Formulir.kt
@@ -2,16 +2,21 @@
 
 package com.example.navigationquest.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import com.example.navigationquest.R
 
 @Composable
 fun FormIsian(
@@ -27,6 +32,9 @@ fun FormIsian(
                     (colorResource(id = R.color.teal_700))
             ) }
         ){ isiRuang ->
-
+            Column(modifier = Modifier.padding(isiRuang),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally)
+            {}
     }
 }
