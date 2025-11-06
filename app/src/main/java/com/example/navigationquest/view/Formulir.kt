fee0d3f0285397ae8a06b3d0c044b2487d6802de
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-06 15:35:44 +0900

    Menambahkan RadioButton untuk opsi jenis kelamin

--- app/src/main/java/com/example/navigationquest/view/Formulir.kt
+++ app/src/main/java/com/example/navigationquest/view/Formulir.kt
@@ -4,11 +4,14 @@ package com.example.navigationquest.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -51,8 +54,20 @@ fun FormIsian(
                 )
                 HorizontalDivider(modifier = Modifier
                     .padding(20.dp)
-                    .width(250.dp), thickness = ,color =
+                    .width(250.dp), thickness = Thickness,color =
                     Color.Red)
+                Row{
+                    jenisK.forEach {
+                        item->
+                        Row(verticalAlignment = Alignment.CenterHorizontally){
+                            RadioButton(
+                                selected = false,
+                                onClick = {item}
+                            )
+                            Text(text = item)
+                        }
+                    }
+                }
             }
     }
 }
