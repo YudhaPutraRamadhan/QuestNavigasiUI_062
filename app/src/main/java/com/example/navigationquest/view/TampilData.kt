eeef11820aad7534b6273c355eaa36256fc035b0
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-06 16:08:01 +0900

    Membuat dan menampilkan data pada halaman Detail

--- app/src/main/java/com/example/navigationquest/view/TampilData.kt
+++ app/src/main/java/com/example/navigationquest/view/TampilData.kt
@@ -1,6 +1,10 @@
 package com.example.navigationquest.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -9,7 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.navigationquest.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,14 +33,32 @@ fun TampilData(
     )
     Scaffold (modifier = Modifier,
         {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.tampil),
-                    color = Color.White
-                )},
-            colors = TopAppBarDefaults(colorResource(id = R.color.teal_700)
+                TopAppBar(
+                    title = {
+                        Text(text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )},
+                colors = TopAppBarDefaults(colorResource(id = R.color.teal_700)
+                    )
                 )
-            )
         }){ isiRuang->
-        
+            Column(modifier = Modifier.padding(isiRuang),
+                verticalArrangement = Arrangement.SpaceBetween) {
+                Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+                        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))) {
+                    items.forEach { item ->
+                        Column{
+                            Text(text = item.first.uppercase(),
+                                fontSize = 16.dp)
+                            Text(text = item.second,
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = FontFamily.Cursive,
+                                fontSize = 22.sp)
+                        }
+                        HorizontalDivider()
+                    }
+                }
+
+            }
+        }
     }
-}
